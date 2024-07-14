cccf21597250205ed561e87ab2793403dec02009
papasgeorge1808 <papasgeorge1808@gmail.com>
2024-07-14 23:15:22 +0300

    Finally build the app

---
+++ src/main/java/web/models/User.java
@@ -0,0 +1,85 @@
+package web.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Not empty!")
+    @Size(min = 3, max = 20, message = "Name should be between 3 and 20 chars")
+    private String name;
+
+    @Column(name = "last_name")
+    @NotEmpty(message = "Not empty!")
+    @Size(min = 3, max = 20, message = "Name should be between 3 and 20 chars")
+    private String lastName;
+
+    @Column(name = "age")
+    @Min(value = 0, message = "Should be 0 or more")
+    @Max(value = 99, message = "Should be 99 or less")
+    private Byte age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public User() {
+
+    }
+
+    public User(Long id, String name, String lastName, Byte age) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+}
